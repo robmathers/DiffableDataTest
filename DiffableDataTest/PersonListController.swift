da6563f39b8c2f5e66a9a7b9e9c7a7c51cd07884
Rob Mathers <robmathers@gmail.com>
2020-07-29 11:09:04 -0400

    Initial commit

---
+++ DiffableDataTest/PersonListController.swift
@@ -0,0 +1,12 @@
+//
+//  PersonListController.swift
+//  DiffableDataTest
+//
+//  Created by Rob Mathers on 2020-07-23.
+//  Copyright © 2020 Rob Mathers. All rights reserved.
+//
+
+import UIKit
+
+class PersonListController: UICollectionViewController {
+}
